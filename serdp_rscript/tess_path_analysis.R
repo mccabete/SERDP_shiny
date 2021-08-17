5d4cfa71d750b7939227ea1bea452c7d888a1639
Tempest McCabe <tmccabe@bu.edu>
2021-08-17 10:28:33 -0400

    A working point-range alternative plot for low N

--- serdp_rscript/tess_path_analysis.R
+++ serdp_rscript/tess_path_analysis.R
@@ -469,7 +469,7 @@ plot_comparison_ggplot <- function(simulated, baseline, title){
   simulated_conf <- list()
   simulated_conf$confidence_points <- c(simulated$conf.low, simulated$conf.high, simulated$predicted)
   simulated_conf$label <- "Simulated Levels"
-  simulated_conf$quant <- "95% Prediction Interval"
+  simulated_conf$quant <- "Prediction Interval"
   simulated_conf <- as.data.frame(simulated_conf)
   
   
@@ -478,28 +478,52 @@ plot_comparison_ggplot <- function(simulated, baseline, title){
   baseline_conf <- list()
   baseline_conf$confidence_points <- c(baseline$conf.low, baseline$conf.high, baseline$predicted)
   baseline_conf$label <- "Baseline"
-  baseline_conf$quant <- "95% Prediction Interval"
+  baseline_conf$quant <- "Prediction Interval 95%"
   
   baseline_conf <- as.data.frame(baseline_conf)
   
-  p <- ggplot() + 
-    theme_classic()+
-    geom_violin(data = simulated_conf, aes(x = confidence_points, y = label, fill = label, alpha = quant)) +
-    geom_violin(data = baseline_conf, aes(x = baseline_conf$confidence_points, y = baseline_conf$label, fill = label, alpha = quant)) +
-    geom_violin(data = simulated, aes(x = simulated$predicted, y = simulated$label, fill = label, alpha = quant)) + 
-    geom_violin(data = baseline, aes(x = baseline$predicted, y = baseline$label, fill = label, alpha = quant)) +
-    geom_point(data = baseline, aes(x = round(mean(baseline$predicted), digits = 3), y = baseline$label[1]), color = "yellow") +
-    geom_text(aes(x = mean(baseline$predicted), y = baseline$label[1]), label = paste("Mean:", round(mean(baseline$predicted), digits = 3)), nudge_x = 4, nudge_y = 0.3, family = "Ariel", size = 3) + 
-    geom_point(data = simulated, aes(x = round(mean(simulated$predicted), digits = 3), y = simulated$label[1]), color = "yellow") +
-    geom_text(aes(x = mean(simulated$predicted), y = simulated$label[1]), label = paste("Mean:", round(mean(simulated$predicted), digits = 3)), nudge_x = 4, nudge_y = 0.3, family = "Ariel", size = 3) + 
-    coord_flip() + 
-    xlab(title) +
-    ylab("") + 
-    scale_fill_discrete(guide = "none")  + 
-    scale_alpha_discrete(range=c(0.5, 1)) + 
-    guides(alpha = guide_legend(override.aes = list(fill = c("grey70", "grey50"))) ) 
+  if(length(baseline$predicted) <= 9){
+    ggplot() + 
+      theme_classic()+
+      geom_pointrange(data = simulated_conf, aes(ymin = min(confidence_points), ymax = max(confidence_points),y = mean(confidence_points), x = label, color = label, linetype = quant)) +
+      geom_pointrange(data = baseline_conf, aes(ymin = min(confidence_points), ymax = max(confidence_points),y = mean(confidence_points), x = label, color = label, linetype = quant)) +
+      geom_pointrange(data = simulated, aes(ymin = min(predicted), ymax = max(predicted),y = mean(predicted), x = label, color = label, linetype = quant)) +
+      geom_pointrange(data = baseline, aes(ymin = min(predicted), ymax = max(predicted),y = mean(predicted), x = label, color = label, linetype = quant)) +
+      #geom_point(data = baseline, aes(x = round(mean(baseline$predicted), digits = 3), y = baseline$label[1])) +
+      geom_text(aes(y = mean(baseline$predicted), x = baseline$label[1]), label = paste("Mean:", round(mean(baseline$predicted), digits = 3)), nudge_x = 0.3, family = "Ariel", size = 3) + 
+      #geom_point(data = simulated, aes(x = round(mean(simulated$predicted), digits = 3), y = simulated$label[1])) +
+      geom_text(aes(y = mean(simulated$predicted), x = simulated$label[1]), label = paste("Mean:", round(mean(simulated$predicted), digits = 3)), nudge_x = 0.3, family = "Ariel", size = 3) + 
+      #scale_linetype_discrete(c("b", "a")) + 
+      ylab(title) +
+      xlab("") + 
+      scale_color_discrete(guide = "none") + 
+      guides(linetype = guide_legend(override.aes = list(shape = c(NA, NA), title = NA)))
+      #scale_alpha_discrete(range=c(0.5, 1)) + 
+     
+    
+  }else{
+    p <- ggplot() + 
+      theme_classic()+
+      geom_violin(data = simulated_conf, aes(x = confidence_points, y = label, fill = label, alpha = quant)) +
+      geom_violin(data = baseline_conf, aes(x = baseline_conf$confidence_points, y = baseline_conf$label, fill = label, alpha = quant)) +
+      geom_violin(data = simulated, aes(x = simulated$predicted, y = simulated$label, fill = label, alpha = quant)) + 
+      geom_violin(data = baseline, aes(x = baseline$predicted, y = baseline$label, fill = label, alpha = quant)) +
+      geom_point(data = baseline, aes(x = round(mean(baseline$predicted), digits = 3), y = baseline$label[1]), color = "yellow") +
+      geom_text(aes(x = mean(baseline$predicted), y = baseline$label[1]), label = paste("Mean:", round(mean(baseline$predicted), digits = 3)), nudge_y = 0.3, family = "Ariel", size = 3) + 
+      geom_point(data = simulated, aes(x = round(mean(simulated$predicted), digits = 3), y = simulated$label[1]), color = "yellow") +
+      geom_text(aes(x = mean(simulated$predicted), y = simulated$label[1]), label = paste("Mean:", round(mean(simulated$predicted), digits = 3)), nudge_x = round(mean(simulated$predicted), digits = 3), nudge_y = 0.3, family = "Ariel", size = 3) + 
+      coord_flip() + 
+      xlab(title) +
+      ylab("") + 
+      scale_fill_discrete(guide = "none")  + 
+      scale_alpha_discrete(range=c(1, 0.5)) + 
+      guides(alpha = guide_legend(override.aes = list(fill = c("grey50", "grey70"), title = NA)) ) 
+    
+    return(p)
+    
+  }
   
-  return(p)
+  
   
   
   
@@ -507,6 +531,11 @@ plot_comparison_ggplot <- function(simulated, baseline, title){
 
 
 
+
+
+
+
+####
 ticks$label <- "Simulated Levels"
 ticks$quant <- "Mean Predictions"
 ticks_conf <- list()
